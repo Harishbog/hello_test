352e0dddb3d7b7b3a5ba4a94b5af874e1bea776e
Harishbog <159771573+Harishbog@users.noreply.github.com>
2024-02-12 16:17:27 +0530

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,9 @@
+def main():
+    # Prompt the user to enter their name
+    name = input("Sreevalli: ")
+
+    # Greet the user
+    print("Hello, " + name + "!")
+
+if __name__ == "__main__":
+    main()
